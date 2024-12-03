71f917ae5ec6ffa8185adb9fdb937cfc6cff1e30
Gaurav Singh <161499206+GauravSingh696@users.noreply.github.com>
2024-12-03 16:18:53 +0530

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded',()=>{
     let historyDiv=document.querySelector('.history');
     let screen=document.querySelector('.screen')
     let buttons=document.querySelectorAll('.btn')
     let history="";
+    let keys= ['0','1','2','3','4','5','6','7','8','9','c','C','+','=','-','*','/','.']
 
-    buttons.forEach(function(button){
-        button.addEventListener('click',function(){
+    buttons.forEach((button)=>{
+        button.addEventListener('click',()=>{
             handleButtonClick(button.innerText);
         });
     });
 
     function handleButtonClick(value) {
-        if(value==='C')
+        if(value==='C' || value=='c')
         {
             clearAll();
         }
@@ -42,27 +43,54 @@ document.addEventListener('DOMContentLoaded',function(){
 
     function appendToScreen(value) {
         screen.textContent+=value;
+        screen.scrollLeft = screen.scrollWidth;
     }
 
     function evaluateExpression() {
         try{
             let expression=screen.textContent;
-            let result=eval(expression)
+            let result=eval(expression);
             
-            result=parseFloat(result.toFixed(5))
-
+            result=parseFloat(result.toFixed(5));
+            
             history=expression+'='+result;
             screen.textContent=result;
+            console.log(typeof(result));
             updateHistory();
+
+            if( result==Infinity || isNaN(result)) {
+                setTimeout(() => {
+                    screen.textContent='';
+                }, 1100);
+            }
         }
 
         catch(error)
         {
             screen.textContent='Error'
+            setTimeout(() => {
+                screen.textContent='';
+            }, 1100);
         }
     }
 
     function updateHistory() {
         historyDiv.textContent=history;
     }
+
+    document.addEventListener('keydown', function(e) {
+        if( keys.includes(e.key)) {
+            handleButtonClick(e.key);
+            console.log(e);
+        }
+
+        else if(e.key=='Backspace') {
+            handleButtonClick('DEL');
+        }
+
+        else if(e.key=="Enter") {
+            e.preventDefault();
+            handleButtonClick('=');
+        }
+    });
 });
